458111a3476a0abe92358f99458b4b8d074a5180
pandassi <lila.ait.ab@gmail.com>
2019-07-05 14:45:47 -0400

    Update

---
+++ js/nav-bar.js
@@ -0,0 +1,18 @@
+var banner = document.getElementsByClassName('tab-nav-container')[0];
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(clickedTab => {
+    // Add onClick event listener on each tab
+    clickedTab.addEventListener('click', () => {
+        // Remove the active class from all the tabs (this acts as a "hard" reset)
+          tabs.forEach(tab => {
+            tab.classList.remove('active');
+        });
+
+        // Add the active class on the clicked tab
+        clickedTab.classList.add('active');
+		const clickedTabBGColor = getComputedStyle(clickedTab).getPropertyValue('color');
+		console.log(clickedTabBGColor);
+		banner.style.backgroundColor = clickedTabBGColor;
+    });
+});
